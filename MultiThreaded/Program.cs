94946595414768ff6f82c22370cdae8468875e5e
Артур Стариков <Gibyte.mail@gmail.com>
2024-03-13 14:44:48 +0300

    refactoring

--- MultiThreaded/Program.cs
+++ MultiThreaded/Program.cs
@@ -1,14 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Drawing;
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+int[] array1 = GenerateRandomArray(100000);
+int[] array2 = GenerateRandomArray(1000000);
+int[] array3 = GenerateRandomArray(10000000);
 
-int[] array = GenerateRandomArray(100000);
-//int[] array = GenerateRandomArray(1000000);
-//int[] array = GenerateRandomArray(10000000);
+MeasureTime(array1, "Array size 100000");
+MeasureTime(array2, "Array size 1000000");
+MeasureTime(array3, "Array size 10000000");
 
-NormalSum(array);
-ThreadsSum(array);
+static void MeasureTime(int[] array, string message)
+{
+    Console.WriteLine("----------------------------------------------");
+    Console.WriteLine(message);
+    Stopwatch stopwatch = new Stopwatch();
+
+    // Normal sum
+    Console.WriteLine("Normal");
+    stopwatch.Start();
+    NormalSum(array);
+    stopwatch.Stop();
+    Console.WriteLine("time: " + stopwatch.ElapsedMilliseconds + "ms");
+    Console.WriteLine();
+    
+    // Threads sum
+    Console.WriteLine("Threads");
+    stopwatch.Reset();
+    stopwatch.Start();
+    ThreadsSum(array);
+    stopwatch.Stop();
+    Console.WriteLine("time: " + stopwatch.ElapsedMilliseconds + "ms");
+    Console.WriteLine();
+    
+    // LINQ sum
+    Console.WriteLine("LINQ");
+    stopwatch.Reset();
+    stopwatch.Start();
+    LINQSum(array);
+    stopwatch.Stop();
+    Console.WriteLine("time: " + stopwatch.ElapsedMilliseconds + "ms");
+}
+
 
 static void NormalSum(int[] array)
 {
@@ -18,23 +49,20 @@ static void NormalSum(int[] array)
     {
         sum += num;
     }
-    Console.WriteLine("Сумма элементов массива: " + sum);
+    Console.WriteLine("Sum: " + sum);
 }
 
 static void ThreadsSum(int[] array)
 {
-    int sum = 0;
-    List<Thread> threads = new List<Thread>();
+    int chunkSize = array.Length / 4; // Размер части массива для каждого потока
+    int[] results = new int[4];
+    Thread[] threads = new Thread[results.Length];
 
-    foreach (int num in array)
+    for (int i = 0; i < results.Length; i++)
     {
-        Thread thread = new((object obj) =>
-        {
-            int element = (int)obj;
-            Interlocked.Add(ref sum, element);
-        });
-        threads.Add(thread);
-        thread.Start(num);
+        int threadIndex = i;
+        threads[i] = new Thread(() => CalculateSum(array, threadIndex, results));
+        threads[i].Start();
     }
 
     foreach (Thread thread in threads)
@@ -42,16 +70,42 @@ static void ThreadsSum(int[] array)
         thread.Join();
     }
 
-    Console.WriteLine("Сумма элементов массива (параллельно с Thread): " + sum);
+    int totalSum = 0;
+    foreach (int result in results)
+    {
+        totalSum += result;
+    }
+    Console.WriteLine("Sum: " + totalSum);
+}
+
+static void CalculateSum(int[] array, int threadIndex, int[] results)
+{
+    int chunkSize = array.Length / results.Length;
+    int startIndex = threadIndex * chunkSize;
+    int endIndex = (threadIndex == results.Length - 1) ? array.Length : (threadIndex + 1) * chunkSize;
+    int sum = 0;
+
+    for (int i = startIndex; i < endIndex; i++)
+    {
+        sum += array[i];
+    }
+
+    results[threadIndex] = sum;
+}
+
+static void LINQSum(int[] array)
+{
+    long sum = array.AsParallel().Sum();
+    Console.WriteLine("Sum: " + sum);
 }
 
 static int[] GenerateRandomArray(int size)
 {
-    Random random = new Random();
+    Random random = new();
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = random.Next(); // Генерация случайного числа
+        array[i] = random.Next();
     }
     return array;
 }
